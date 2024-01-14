7e821eb1b141989692d9570236da20bc8d2ed4fb
eldinor <eldinor@gmail.com>
2024-01-14 17:00:59 +0300

    good

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-import { Num } from './Num'
+import {RandomEffector} from "./core";
+import { RadialCloner as RadialCloner } from "./radialCloner";
+import { MatrixCloner as MatrixCloner } from "./matrixCloner";
+import { LinearCloner as LinearCloner } from "./linearCloner";
+import { ObjectCloner as ObjectCloner } from "./objectCloner";
 
-export { Num }
+export {RandomEffector, RadialCloner, MatrixCloner, LinearCloner,ObjectCloner}
+
+/*
+
+export const RandomEffector = _RandomEffector;
+export const RadialCloner = _RadialCloner;
+export const MatrixCloner = _MatrixCloner;
+export const LinearCloner = _LinearCloner;
+export const ObjectCloner = _ObjectCloner;
+*/
